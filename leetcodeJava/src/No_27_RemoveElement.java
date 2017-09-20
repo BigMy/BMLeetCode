7da4c12370e8a8daf2498e55ea3e9723723cbb6f
BigMy <bigmy@bigmy.me>
2017-09-20 23:42:11 +0800

    第27题

---
+++ leetcodeJava/src/No_27_RemoveElement.java
@@ -0,0 +1,19 @@
+public class No_27_RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+
+
+        
+
+        int newArrayIdx = 0;
+
+        for (int idx = 0; idx < nums.length; idx ++) {
+            if (nums[idx] != val) {
+                nums[newArrayIdx] = nums[idx];
+                newArrayIdx ++;
+            }
+        }
+
+        return newArrayIdx;
+    }
+}
