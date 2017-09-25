5b2ff6fe3d41825ee9809bb19af4be774bd0e0ab
BigMy <bigmy@bigmy.me>
2017-09-25 21:26:49 +0800

    第216题临时备份

---
+++ leetcodeJava/src/No_216_M_Combination_Sum_III.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_216_M_Combination_Sum_III {
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        int[] candidates = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int idx = 0; idx < candidates.length - 2; idx ++) {
+
+        }
+        return null;
+    }
+
+    static void apppendCombination(List<List<Integer>> result,
+                                   List<Integer> combinationList,
+                                   int[] candidates,
+                                   int candidateIdx,
+                                   int n,
+                                   int depth,
+                                   int k) {
+
+
+    }
+
+}
