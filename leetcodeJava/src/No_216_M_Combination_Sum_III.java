9ae2d8ab889a5d629e9fd1f74edb67d4004b0490
BigMy <bigmy@bigmy.me>
2017-09-25 22:50:59 +0800

    第216题

--- leetcodeJava/src/No_216_M_Combination_Sum_III.java
+++ leetcodeJava/src/No_216_M_Combination_Sum_III.java
@@ -3,7 +3,20 @@ import java.util.List;
 
 public class No_216_M_Combination_Sum_III {
 
-    public List<List<Integer>> combinationSum3(int k, int n) {
+
+    public static void main(String[] args) {
+        int k = 3;
+        int n = 50;
+        System.out.println("input: k=" + k + " n=" + n);
+        List<List<Integer>> result = combinationSum3(k, n);
+
+        for (List<Integer> combination : result) {
+            System.out.println(combination.toString());
+        }
+    }
+
+
+    public static List<List<Integer>> combinationSum3(int k, int n) {
 
         List<List<Integer>> result = new ArrayList<>();
 
@@ -11,19 +24,41 @@ public class No_216_M_Combination_Sum_III {
 
         for (int idx = 0; idx < candidates.length - 2; idx ++) {
 
+            if (candidates[idx] < n) {
+                List<Integer> combinationList = new ArrayList<>();
+                combinationList.add(candidates[idx]);
+                appendCombination(result, combinationList, candidates, idx + 1, candidates[idx] , n, 1, k);
+            }
+
         }
-        return null;
+        return result;
     }
 
-    static void apppendCombination(List<List<Integer>> result,
-                                   List<Integer> combinationList,
-                                   int[] candidates,
-                                   int candidateIdx,
-                                   int n,
-                                   int depth,
-                                   int k) {
+    static void appendCombination(List<List<Integer>> result,
+                                  List<Integer> combinationList,
+                                  int[] candidates,
+                                  int candidateIdx,
+                                  int sum,
+                                  int n,
+                                  int depth,
+                                  int k) {
 
-
+        if (depth == k) {
+            if (sum == n) {
+                result.add(combinationList);
+            }
+            return;
+        }
+        for (int idx = candidateIdx; idx < candidates.length; idx ++) {
+            int currSum = sum;
+            if (candidates[idx] <= n - currSum) {
+                List<Integer> nextCombination = new ArrayList<>(combinationList);
+                nextCombination.add(candidates[idx]);
+                currSum += candidates[idx];
+                appendCombination(
+                        result, nextCombination, candidates, idx + 1, currSum, n, depth + 1, k);
+            }
+        }
     }
 
 }
