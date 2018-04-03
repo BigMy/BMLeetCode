3ba14b327a712f383f50078bc7724ae5f76f116a
BigMy <bigmy@bigmy.me>
2018-04-03 10:25:28 +0800

    temp op
    temp op

---
+++ leetcodeJava/src/No_79_M_Word_Search.java
@@ -0,0 +1,16 @@
+public class No_79_M_Word_Search {
+
+    public boolean exist(char[][] board, String word) {
+
+        if (board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+
+
+        return true;
+    }
+
+    private void backStrack(char[][] board, String word,) {
+
+    }
+}
